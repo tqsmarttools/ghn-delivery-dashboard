e16272155a6b9f3e569df4d3b859f1b22b71a64a
tqsmarttools <tqsmarttools@gmail.com>
2026-06-21 17:26:32 +0700

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const cacheName = "ghn-dashboard-v1";
+const assets = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./data/sample-orders.json",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))),
+    ),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html"))),
+  );
+});
+
